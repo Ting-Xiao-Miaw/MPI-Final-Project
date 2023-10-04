5a1a2bc6c169cd65d1a285c31be19c3243effacf
Ting-Xiao Miaw <96478096+Ting-Xiao-Miaw@users.noreply.github.com>
2023-10-04 11:44:54 +0800

    Add files via upload

---
+++ stdio_ver/decoder_main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
+#include <unistd.h>
+int main(int argc, char ** argv){
+        int rank, size;
+        MPI_Init(&argc, &argv);
+        MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
+        int N = 7, K = 4;
+        int R = N - K;
+        int *H;
+        int *Num_Matrix;
+        int *Data;
+        FILE *fptr;
+        char *filename = "rev_data.txt";
+        if(rank == 0){
+                /* File Pointer */
+                fptr = fopen(filename, "w");
+                if( fptr == NULL ){
+                    printf("Can't open file.");
+                    exit(1);
+                }
+                // Input a H matrix
+                printf("H matrix:\n");
+                H = malloc(sizeof(int)*(R*N));
+                for(int i=0; i<N; i++){
+                        for(int j=0; j<R; j++){
+                                scanf("%d", &H[i*R+j]);
+                        }
+                }
+                printf("Encoded data:\n");
+                scanf("%d", Num_Matrix);
+                Data = malloc(sizeof(int)*(N*K)*(*Num_Matrix));
+                for(int t = 0; t < *Num_Matrix; t++){
+                    for(int i=0; i<K; i++){
+                            for(int j=0; j<N; j++){
+                                    scanf("%d", &Data[t*28+i*N+j]);
+                            }
+                    }
+                }
+        }
+        if(rank!=0){
+            H = malloc(sizeof(int) * (R*N));
+        }
+        // Broadcast the H matrix
+        MPI_Bcast(H, R*N, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(Num_Matrix, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        for(int t = 0; t < *Num_Matrix; t++){
+            int *row;
+            int *dec_row;
+            row = malloc(sizeof(int) * N);
+            MPI_Scatter(&Data[t*28], N, MPI_INT, row, N, MPI_INT, 0, MPI_COMM_WORLD);
+            // Each worker decodes the row to the new one
+            dec_row = malloc(sizeof(int) * R);
+            for(int i = 0; i < R; i++){
+                dec_row[i] = 0;
+                for(int j = 0; j < N; j++)
+                    dec_row[i] += row[j] * H[j*R+i];
+                dec_row[i] = dec_row[i]%2;
+            }
+            // Compare Syndrome, do Error Correction
+            for(int i = 0; i < N; i++){
+                int cmp = memcmp(dec_row, &H[i*R], sizeof(int)*R);
+                if(cmp == 0){
+                    //printf("Data[%d] at rank[%d]: Match error pattern %d.\n", t, rank, i+1);
+                    row[i] = (row[i] + 1)%2;
+                }
+            }
+            // Gather decoded rows to the manager, get an matrix DD
+            int *DD;
+            DD = malloc(sizeof(int)*(N*K));
+            MPI_Gather(row, N*(K/size), MPI_INT, DD, N*(K/size), MPI_INT, 0, MPI_COMM_WORLD);
+            if(rank == 0){
+                // Extract the original data D
+                int *D;
+                D = malloc(sizeof(int)*(K*K));
+                for(int i=0; i<K; i++){
+                    for(int j=R; j<N; j++){
+                        D[i*K+j-R] = DD[i*N+j];
+                    }
+                }
+                /* For loop for read 8-bit, write it to the txt file in char mode */
+                for( int i = 0; i < 2; i++ ){
+                    /* Binary to ASCII int */
+                    int r = 0;
+                    for( int j = 0; j < 8; j++ ){
+                        /* p, caculate pow(2,7-j) */
+                        int p = 1;
+                        for(int k = 0; k < 7-j; k++ ){
+                            p = p*2;
+                        }
+                        r += D[j+i*8] * p;
+                    }
+                    fprintf( fptr, "%c", r );
+                } 
+            }
+        }
+        if(rank == 0){
+            fclose(fptr);
+            printf("File '%s' created.\n", filename);
+        }
+        MPI_Finalize();
+}
---
+++ stdio_ver/encoder_main.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <unistd.h>
+#define MATRIX_SIZE 4
+
+void decimalToBinary(int num, int* onebyte) {
+    if (num == 0) {
+        for(int i = 0; i < 8; i++)
+            onebyte[i] = 0;
+        return;
+    }
+   // Stores binary representation of number.
+   int binaryNum[8]; 
+   int i=0;
+
+   for ( ;num > 0; ){
+      binaryNum[i++] = num % 2;
+      num /= 2;
+   }
+   int b = 0;
+   for (int z = 0;z < 8 - i; z++)
+      onebyte[b++] = 0;
+   for (int j = i-1; j >= 0; j--){
+      onebyte[b++] = binaryNum[j];
+   }
+}
+
+int main(int argc, char ** argv) {
+    //====================================================================================
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    int N = 7, K = 4;
+    int *G;
+    int *Data;
+    int *Num_Matrix;
+    Num_Matrix = malloc(sizeof(int));
+    FILE* file;
+    char *filename = "data.txt";
+    //====================================================================================
+    if(rank == 0){
+        // Input a G matrix
+        //printf("G matrix:\n");
+        G = malloc(sizeof(int)*(N*K));
+        for(int i=0; i<K; i++){
+            for(int j=0; j<N; j++){
+                scanf("%d", &G[i*N+j]);
+            }
+        }
+        //================================================================================
+        // Open binary file, then read into the variable "data"
+        file = fopen(filename, "rb");
+        if (file == NULL) {
+            printf("Cannot open the file.\n");
+            return 1;
+        }
+        fseek(file, 0, SEEK_END);
+        long file_size = ftell(file);
+        rewind(file);
+        unsigned char* data = (unsigned char*)malloc(file_size);
+        fread(data, 1, file_size, file);
+        fclose(file);
+        // Slice data into 16-bit matrix
+        int num_matrices = (file_size + 1)/2;
+        Num_Matrix = &num_matrices;
+        Data = malloc((K*K)* num_matrices * sizeof(int*));
+        int** matrices = (int**)malloc(num_matrices * sizeof(int*));
+        for (int i = 0; i < num_matrices; i++) {
+            unsigned char matrix[MATRIX_SIZE][MATRIX_SIZE];
+            int byte_index = i * 2;
+            for (int row = 0; row < MATRIX_SIZE; row++) {
+                for (int col = 0; col < MATRIX_SIZE; col++) {
+                    int byte_value = data[byte_index + col + (row * MATRIX_SIZE)];
+                    matrix[row][col] = byte_value;
+                }
+            }
+            // Implicit covert matrix from char to int
+            int* int_matrix = (int*)malloc(MATRIX_SIZE * MATRIX_SIZE * sizeof(int));
+            for (int row = 0; row < MATRIX_SIZE; row++) {
+                for (int col = 0; col < MATRIX_SIZE; col++) {
+                    int_matrix[row * MATRIX_SIZE + col] = matrix[row][col];
+                }
+            }
+            matrices[i] = int_matrix;
+            decimalToBinary(int_matrix[0], &Data[i*16]);
+            decimalToBinary(int_matrix[1], &Data[i*16+8]);
+        }
+        //================================================================================
+    }//rank 0
+    if(rank != 0){
+        G = malloc(sizeof(int) * (N*K));
+    }
+    // Broadcast the G matrix
+    MPI_Bcast(G, N*K, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(Num_Matrix, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if(rank == 0)
+        printf("%d\n", *Num_Matrix);
+    for(int i = 0; i < *Num_Matrix; i++){
+        int *row;
+        int *enc_row;
+        int *E;
+        row = malloc(sizeof(int) * (K*K/size));
+        MPI_Scatter(&Data[i*16], K*K/size, MPI_INT, row, K*K/size, MPI_INT, 0, MPI_COMM_WORLD);
+        // each worker encodes the row to the new one
+        enc_row = malloc(sizeof(int) * N);
+        for(int j = 0; j < N; j++){
+            enc_row[j] = 0;
+            for(int k = 0; k < K; k++)
+                enc_row[j] += row[k] * G[k*N+j];
+            enc_row[j] = enc_row[j]%2;
+        }
+        // Gather encoded rows to the manager, get an encoded matrix E
+        E = malloc(sizeof(int)*(N*K));
+        MPI_Gather(enc_row, N*(K/size), MPI_INT, E, N*(K/size), MPI_INT, 0, MPI_COMM_WORLD);
+        if(rank == 0){
+            //printf("Data[%d] E Matrix:\n", i);
+            for(int i=0; i<K; i++){
+                 for(int j=0; j<N; j++){
+                     printf("%d ", E[i*N+j]);
+                 }
+                 printf("\n");
+            }
+            // send out each 16 bits data
+        }
+    }
+    MPI_Finalize();
+    return 0;
+}
